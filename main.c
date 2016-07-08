406eea20b71fea6bf3f3d758eb8d1a66decf63e7
inochi108 <inochi108@hotmail.com>
2016-07-08 15:35:38 -0700

    Initialized Project

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Jowy
+ *
+ * Created on July 8, 2016, 3:32 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("hello world");
+    return (EXIT_SUCCESS);
+}
+
